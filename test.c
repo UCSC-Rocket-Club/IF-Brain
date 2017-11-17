351641b15f9ec9c1482d654ea455cbe3ea0ff5e8
Johnson Le <jonle@ucsc.edu>
2017-11-16 20:54:12 -0800

    makefile and gcc bash tut for bill

--- test.c
+++ test.c
@@ -1,9 +1,13 @@
-//john and bill
+///////////////////////////////////////////////
+// Names: Johnson | Bill
+// Date: 11/16/2017
+// Description: for segment 8
+///////////////////////////////////////////////
+
 #include<stdio.h>
+#include<stdlib.h>
 
-void main(){
-
-printf("Test");
-
-
+int main(int argc, char* argv[]){
+     printf("hello world\n");
+     return EXIT_SUCCESS;
 }
