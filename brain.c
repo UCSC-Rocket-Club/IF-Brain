4e7472b763f87fe51cc246f00abbbc982c0b9a46
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-11-16 16:20:45 -0800

    Segment 6-7

--- brain.c
+++ brain.c
@@ -18,9 +18,12 @@ macro definitions
 void startSequence();
 void feedbackLoop(int initalData[]); 
 bool cleanUp();
-int ADASRead();
-void ADASControl(int f);
-
+float ADASRead();
+void ADASControl(float f);
+float * accRead();
+float * gyroRead();
+float * kalmanFilter(float data[]);
+float * derivation(float valueSum[], float valueNew[], float dt);
 
 /*  Segment 1 */
 /* Start Sequence */
@@ -36,8 +39,39 @@ void startSequence(){
 /* Feedback loop start */ 
 
 void feedbackLoop(initalData){
+	/* global variables */
+	// kalman filter state variables 
+	// gyroData
+	// accData
+	// sums of accData
+	// sums of gyroData
+	// time var
 
 	while(feedbackConditon){
+		/* Segment 5 */
+		/* Sensor interface */
+		
+		// set data from accRead() into readable data
+		// set data from gyroRead() into readable data
+
+
+		/* Segment 6 */
+		/* Kalman Filter */
+		// Pass matrix and state variables into Kaman filter
+		{stateVars} = kalmanFilter(accData);
+		{stateVars} = kalmanFilter(gyroData);
+
+		/* Segment 7 */
+		/* Calculate dt */
+		// talk with electrical to find best way to do this
+
+		/* Segment 8 */
+		/* 3.D. Calculations */
+		float vLinear[] += derivation(vLinear, accLinear, dt);
+		float pLinear[] += derivation(pLinear, vLinear, dt);
+
+		float vAngular[] += derivation(vAngular, accAngular, dt);
+		float angAngular[] += derivation(angAngular, vAngular, dt);  
 
 	}
 
@@ -46,18 +80,26 @@ void feedbackLoop(initalData){
 	while (success == cleanUp() == false) {}
 }
 
+/* Segment 4 */
+/* Retract ADAS fins */
 bool cleanUp(){
 	// see how open ADAS is
-	fractionOfOpen = ADASRead();
+	float fractionOfOpen = ADASRead();
 	// retract ADAS fins
-	ADASControl = ADASControl(-fractionOfOpen)
+	ADASControl(-fractionOfOpen)
 
-	success = ADASRead();
-	if(success == 0){
+	float success = ADASRead();
+	if(success == 0.0){
 		return true
 	}else{
 		// FIX IT
 	}
+}
+
+/* Segment 9 */
+/* Derivation Function */
+float * derivation(float valueSum[], float valueNew[], dt){
+
 }
 
 
